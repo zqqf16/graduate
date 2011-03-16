66ddabebc07a1f397710317a356b69ef7cacb0be
zorro <zqqf16@gmail.com>
2011-03-16 19:13:13 +0800

    ip_package

---
+++ ip_package.h
@@ -0,0 +1,21 @@
+#ifndef _IP_PACKAGE_H_
+#define _IP_PACKAGE_H_
+
+typedef struct
+{
+		unsigned char 	version;	/*low 4 bits are useful, highters are 0*/
+		/*head_len some times more than 5, to make it sample, I ignore it */
+		unsigned char 	head_len;	/*low 4 bits are useful, highters are 0*/
+		unsigned char 	tos;		
+		unsigned short 	total_len;
+		unsigned short	id;
+		unsigned char	flag;		/*low 3 bits are useful, highters are 0*/
+		unsigned short	offset;		/*low 13 bits are useful, highters are 0*/
+		unsigned char	ttl;		
+		unsigned char 	protocol;	/*not all protocols are supportted, just some well-known*/
+		unsigned short	head_sum;
+		unsigned char 	src[4];
+		unsigned char 	dest[4];
+		void			*data;
+} Ip_package;
+
